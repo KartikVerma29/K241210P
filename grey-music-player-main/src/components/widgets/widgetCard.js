df4ade50012022590cdc32427341d2c5084f96cc
kartik <kartikverma29022004@gmail.com>
2025-04-17 21:48:42 +0530

    fixed few errors

--- grey-music-player-main/src/components/widgets/widgetCard.js
+++ grey-music-player-main/src/components/widgets/widgetCard.js
@@ -4,43 +4,50 @@ import WidgetEntry from "./widgetEntry";
 import { IconContext } from "react-icons";
 import { FiChevronRight } from "react-icons/fi";
 
-export default function WidgetCard({ title, similar, featured, newRelease }) {
-  console.log(
-    "similar",
-    similar,
-    "featured",
-    featured,
-    "newRelease",
-    newRelease
-  );
+export default function WidgetCard({ title, similar = [], featured = [], newRelease = [] }) {
+  console.log("similar", similar, "featured", featured, "newRelease", newRelease);
+
+  const renderEntries = () => {
+    if (similar.length > 0) {
+      return similar.map((artist) => (
+        <WidgetEntry
+          key={artist?.id || artist?.name}
+          title={artist?.name}
+          subtitle={`${artist?.followers?.total || 0} Followers`}
+          image={artist?.images?.[2]?.url}
+        />
+      ));
+    }
+
+    if (featured.length > 0) {
+      return featured.map((playlist) => (
+        <WidgetEntry
+          key={playlist?.id || playlist?.name}
+          title={playlist?.name}
+          subtitle={`${playlist?.tracks?.total || 0} Songs`}
+          image={playlist?.images?.[0]?.url}
+        />
+      ));
+    }
+
+    if (newRelease.length > 0) {
+      return newRelease.map((album) => (
+        <WidgetEntry
+          key={album?.id || album?.name}
+          title={album?.name}
+          subtitle={album?.artists?.[0]?.name || ""}
+          image={album?.images?.[2]?.url}
+        />
+      ));
+    }
+
+    return null;
+  };
+
   return (
     <div className="widgetcard-body">
       <p className="widget-title">{title}</p>
-      {similar
-        ? similar.map((artist) => (
-            <WidgetEntry
-              title={artist?.name}
-              subtitle={artist?.followers?.total + " Followers"}
-              image={artist?.images[2]?.url}
-            />
-          ))
-        : featured
-        ? featured.map((playlist) => (
-            <WidgetEntry
-              title={playlist?.name}
-              subtitle={playlist?.tracks?.total + " Songs"}
-              image={playlist?.images[0]?.url}
-            />
-          ))
-        : newRelease
-        ? newRelease.map((album) => (
-            <WidgetEntry
-              title={album?.name}
-              subtitle={album?.artists[0]?.name}
-              image={album?.images[2]?.url}
-            />
-          ))
-        : null}
+      {renderEntries()}
       <div className="widget-fade">
         <div className="fade-button">
           <IconContext.Provider value={{ size: "24px", color: "#c4d0e3" }}>
