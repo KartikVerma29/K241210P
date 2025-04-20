f694f14cf09d4d3c20ae0ba954d9bb9f8fa11b22
kartik <kartikverma29022004@gmail.com>
2025-04-20 20:28:42 +0530

    yt & spofity apis are working

--- grey-music-player-main/src/components/sidebar/index.js
+++ grey-music-player-main/src/components/sidebar/index.js
@@ -21,14 +21,8 @@ export default function Sidebar() {
     <div className="sidebar-container">
       <img src={image} className="profile-img" alt="profile" />
       <div>
-        <SidebarButton title="Feed" to="/feed" icon={<MdSpaceDashboard />} />
-        <SidebarButton title="Trending" to="/trending" icon={<FaGripfire />} />
         <SidebarButton title="Player" to="/player" icon={<FaPlay />} />
-        <SidebarButton
-          title="Favorites"
-          to="/favorites"
-          icon={<MdFavorite />}
-        />
+        
         <SidebarButton title="Library" to="/" icon={<IoLibrary />} />
       </div>
       <SidebarButton title="Sign Out" to="" icon={<FaSignOutAlt />} />
--- grey-music-player-main/src/screens/home/index.js
+++ grey-music-player-main/src/screens/home/index.js
@@ -3,11 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "../../components/sidebar";
 import { setClientToken } from "../../spotify";
 import Login from "../auth/login";
-import Favorites from "../favorites";
-import Feed from "../feed";
 import Library from "../library";
 import Player from "../player";
-import Trending from "../trending";
 import "./home.css";
 
 export default function Home() {
@@ -36,10 +33,7 @@ export default function Home() {
         <Sidebar />
         <Routes>
           <Route path="/" element={<Library />} />
-          <Route path="/feed" element={<Feed />} />
-          <Route path="/trending" element={<Trending />} />
           <Route path="/player" element={<Player />} />
-          <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </div>
     </Router>
